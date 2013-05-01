2752f288f3e0af6f5b277cba4ad222a8a68b48a7
Paul Mucur <p.mucur@nature.com>
2013-05-01 14:15:19 +0100

    Add codox.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/mudge/riveted"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.ximpleware/vtd-xml "2.11"]]
+  :dependencies [[org.clojure/clojure "1.5.1"] [com.ximpleware/vtd-xml "2.11"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.0.1"]]}})
+                   :plugins [[lein-midje "3.0.1"] [codox "0.6.4"]]}}
+  :codox {:src-dir-uri "https://github.com/mudge/riveted/blob/master"
+          :src-linenum-anchor-prefix "L"})
