0f286b42f642a5623c31513c0ccbc08f13721636
Paul Mucur <p.mucur@nature.com>
2013-05-02 17:09:49 +0100

    Relax Clojure support and enable matrix builds.

--- project.clj
+++ project.clj
@@ -3,8 +3,14 @@
   :url "https://github.com/mudge/riveted"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"] [com.ximpleware/vtd-xml "2.11"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [com.ximpleware/vtd-xml "2.11"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.0.1"] [codox "0.6.4"]]}}
+                   :plugins [[lein-midje "3.0.1"]
+                             [codox "0.6.4"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+             :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :codox {:src-dir-uri "https://github.com/mudge/riveted/blob/master"
           :src-linenum-anchor-prefix "L"})
